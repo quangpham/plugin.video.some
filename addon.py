e731323cc7df5f95b0c626b0b686432121f7146d
Your Name <you@example.com>
2014-09-11 09:38:47 +0300

    add folder

--- addon.py
+++ addon.py
@@ -14,46 +14,50 @@ def httpgetjson(url):
 	response = urllib2.urlopen(url)
 	return json.load(response)
 
+def format_menu_item(item):
+	_item = {}
+	_item['label'] = item['label']
+
+	if item.get('is_playable'):
+		_item['is_playable'] = item['is_playable']
+
+	if item.get('path'):
+		_item['path'] = item['path']
+
+	if item.get('youtube_id'):
+		_item['path'] = "plugin://plugin.video.youtube/?action=play_video&videoid=" + item['youtube_id']
+		_item['is_playable'] = True
+
+	if item.get('folder_id'):
+		_item['path'] = plugin.url_for('show_folder_content', folderid=item['folder_id'])
+
+	return _item
+
+
+
 @plugin.route('/')
 def main_menu():
-	
 	data = httpgetjson('http://localhost:3000/items.json')
-
+	
 	items = []
 	for item in data['items']:
-		_item = {}
-		_item['label'] = item['label']
-
-		if item.get('is_playable'):
-			_item['is_playable'] = item['is_playable']
-
-		if item.get('path'):
-			_item['path'] = item['path']
-
-		if item.get('youtube_id'):
-			_item['path'] = "plugin://plugin.video.youtube/?action=play_video&videoid=" + item['youtube_id']
-			_item['is_playable'] = True
-
-		items.append(_item)
+		items.append(format_menu_item(item))
 
 	return items
 
-@plugin.route('/subjects/')
-def show_subjects():
-	api = AcademicEarth()
-	subjects = api.get_subjects()
-	items = [
-		{
-		'label': subject.name,
-		'path': plugin.url_for('show_subject_info', url=subject.url),
-		} for subject in subjects
-	]
-	sorted_items = sorted(items, key=lambda item: item['label'])
-	return sorted_items
 
-@plugin.route('/subjects/<url>/')
-def show_subject_info(url):
-	pass
+
+@plugin.route('/folders/<folderid>/')
+def show_folder_content(folderid):
+	data = httpgetjson('http://localhost:3000/folder1/folder.json')
+
+	items = []
+	for item in data['items']:
+		items.append(format_menu_item(item))
+
+	return items
+
+
 
 if __name__ == '__main__':
 	plugin.run()
